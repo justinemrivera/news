a42975938dce60966a655136b421d2ff4aa4505e
Rafael GPL <rafaellopez@reps2.com>
2021-11-02 18:16:37 -0700

    Added reqs and gitignore

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/justine0025/COde/SDGKU/news/venv/bin/python3
+#!/Users/rafael/Code/SDGKU/temp/news/venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
